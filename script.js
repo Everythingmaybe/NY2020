394a99b0895da01872bcfefb69a139d630622e83
Everythingmaybe <55mixxx55@gmail.com>
2022-01-06 17:53:34 +0600

    Add text animation. Add settings panel.

--- script.js
+++ script.js
@@ -6,37 +6,44 @@ let HEIGHT = canvas.height = window.innerHeight;
 
 document.body.append(canvas);
 
-window.addEventListener('resize', () => {
-    WIDTH = canvas.width = window.innerWidth;
-    HEIGHT = canvas.height = window.innerHeight;
-    FONT_SIZE = WIDTH / 25;
-})
-
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 
-let WIND = 0;
-let WIND_POWER = 1;
+/** SETTINGS **/
+/** COLORS **/
+let BACKGROUND_COLOR = 'black';
 
-const BACKGROUND_COLOR = 'black';
+let TREE_COLOR = 'white';
+let TREE_SHADOW_COLOR = 'blue';
 
-const TEXT = 'С Новым 2022 Годом!';
+let SNOWFLAKE_COLOR = 'white';
+let SNOWFLAKE_SHADOW_COLOR = 'blue';
 
-let FONT_COLOR_HLS = 0;
-let FONT_SIZE = WIDTH / 25;
+/** TEXT **/
+let TEXT = 'С Новым 2022 Годом!';
+let TEXT_INPUT_INTERVAL = 150;
+let TEXT_INPUT_DELAY = 1000;
 const FONT_FAMILY = 'Press Start 2P';
 
-const TREE_COLOR = 'white';
-const TREE_SHADOW_COLOR = 'blue';
+/** SNOW **/
+let SNOWFLAKES_COUNT = 100;
 
-const SNOWFLAKES_COUNT = 100;
-const SNOWFLAKE_COLOR = 'white';
-const SNOWFLAKE_SHADOW_COLOR = 'blue';
+/** WIND **/
+let WIND_POWER = 1;
 
+/** TREE MAPS **/
 const FIRST_TREE_MAP = {middle: {length: 10, size: 4, right: {length: 30, left: {length: 20, left: {length: 15, left: {length: 20, left: {length: 10, right: {length: 25, right: {length: 20, left: {length: 1, size: 2, left: {length: 20, right: {length: 38, right: {length: 8}}}}, middle: {length: 5, left: {length: 1, size: 2, left: {length: 19, right: {length: 30, right: {length: 8, right: {length: 8}}}}}, middle: {length: 5, left: {length: 1, size: 2, left: {length: 17, right: {length: 20, right: { length: 1 }}}}, middle: {length: 12, left: { length: 8 }}}}}}}}}, middle: {length: 12, left: {length: 10, right: {size: 2, length: 20, left: {length: 15,}}, middle: {length: 20, left: {length: 25, size: 2, right: {length: 22, left: {length: 10, right: {length: 30}}}}, middle: {length: 10, left: {length: 15, right: { length: 25 }}, middle: {length: 15, right: {length: 20, middle: {length: 30, size: 2, left: {length: 30}}, left: {length: 5, right: {length: 24, size: 2, left: {length: 24, left: {length: 10, right: {length: 8, right: {length: 1}}}}}, middle: {length: 5, right: {length: 18, size: 2, left: {length: 15, left: {length: 5, left: {length: 1, left: {length: 5}}}}}, middle: {length: 10, left: {length: 25, right: {length: 5, right: {length: 5}}}}}}}}}}}}}}}};
 const SECOND_TREE_MAP = {middle: {length: 10, size: 2, left: {length: 17, middle: {length: 0}}}};
 const THIRD_TREE_MAP = {middle: {length: 10, size: 5, left: {length: 18, right: {length: 50, right: {length: 20, size: 4, left: {length: 16, right: {length: 50, left: {length: 30, middle: {length: 13}, left: {length: 28, size: 2, right: {length: 1, right: {length: 24, left: {length: 20, right: {length: 1}}}, middle: {length: 5, right: {length: 18, left: {length: 30,}}, middle: {length: 5, right: {length: 12, left: {length: 18, left: {length: 10}}}, middle: {length: 14}}}}}}}}}, middle: {length: 20,/**/ left: {length: 20, right: {length: 10, middle: {length: 28, left: {length: 1}}, left: {length: 38, size: 4, right: {length: 25, left: {length: 25}, middle: {length: 20, size: 2, left: {length: 20, right: {length: 5}}}}}}}, middle: {length: 20, right: {length: 25, size: 2, left: {length: 25}}, middle: {length: 7, right: {length: 14, size: 2, left: {length: 15}}, middle: {length: 50, left: {length: 10, size: 4, right: {length: 20, size: 2}, middle: {length: 20, right: {length: 10, left: {length: 20, size: 2, right: {length: 20, right: {length: 10,}}, middle: {length: 8, right: {length: 20,}}}, middle: {length: 18}}}}, middle: {length: 5, right: {length: 20, left: {length: 25, middle: {length: 30, right: {length: 20, left: {length: 5}}}, left: {length: 15, size: 4, right: {length: 22, left: {length: 25, right: {length: 20, size: 3}}, middle: {length: 45, left: {length: 10, size: 2, right: {length: 15}}, middle: {length: 15, right: {length: 10}, middle: {length: 30, star: true}}}}}}}}}}}}}}}};
 const FOURTH_TREE_MAP = {middle: {length: 10, size: 4, left: {length: 30, right: {length: 30, right: {length: 1, right: {length: 10, left: {length: 10, size: 2, right: {length: 10, left: {length: 28, right: {length: 15, left: {length: 15}, middle: {length: 10, right: {length: 6}}}}}}, middle: {length: 5, right: {length: 15, left: {length: 15, left: {length: 20, right: {length: 1, right: {length: 15, left: {length: 30}}}, middle: {length: 10, right: {length: 25, left: {length: 8, size: 3}, middle: {length: 25, size: 2, left: {length: 5}, right: {length: 8, left: {length: 10, right: {length: 10}}}}}}}}}, middle: {length: 14, size: 2, left: {length: 26, right: {length: 14}}}}}, middle: {length: 50, right: {length: 15, size: 3, left: {length: 25, right: {length: 30}}}, left: {length: 10, right: {length: 30, right: {length: 7, size: 2, left: {length: 15, right: {length: 10}}}, middle: {length: 20, right: {length: 8, size: 2, left: {length: 6, right: {length: 10}}}, middle: {length: 17, size: 2, right: {length: 20}}}, left: {length: 14, right: {length: 10, middle: {length: 20}, left: {length: 25, size: 2, right: {length: 10, left: {length: 20, left: {length: 10, left: {length: 10, right: {length: 10, right: {length: 10}}}}}}}}, middle: {length: 43}}}}}},}}}};
+
+/** VARIABLES **/
+let WIND = 0;
+let TEXT_LENGTH = 0;
+let SYMBOL_TIME = 0;
+let SHOW_SYMBOL = true;
+let TEXT_TIME = 0;
+let FONT_COLOR_HLS = 0;
 
 class TreeNode {
     left;
@@ -131,13 +138,13 @@ class Snowflake {
 
 function step(timestamp) {
     render();
-    update();
+    update(timestamp);
     requestAnimationFrame(step);
 }
 
 requestAnimationFrame(step);
 
-const snowflakes = createSnow(SNOWFLAKES_COUNT);
+let snowflakes = createSnow(SNOWFLAKES_COUNT);
 const trees = [
     createTree(new TreeNode(30, 7), FIRST_TREE_MAP),
     createTree(new TreeNode(30, 7), SECOND_TREE_MAP),
@@ -145,11 +152,26 @@ const trees = [
     createTree(new TreeNode(30, 7), FOURTH_TREE_MAP),
 ];
 
-function update() {
+function update(timestamp) {
     snowflakes.forEach((item) => item.update());
     trees.forEach((item) => item.update());
     WIND += 0.6;
+    updateText(timestamp)
+}
+
+function updateText(timestamp) {
     FONT_COLOR_HLS += 0.6;
+    if (timestamp - SYMBOL_TIME >= 500) {
+        SHOW_SYMBOL = !SHOW_SYMBOL;
+        SYMBOL_TIME = timestamp;
+    }
+
+    if (timestamp - TEXT_TIME >= TEXT_INPUT_INTERVAL && TEXT_INPUT_DELAY <= timestamp) {
+        TEXT_TIME = timestamp;
+        if (TEXT_LENGTH < TEXT.length) {
+            TEXT_LENGTH++
+        }
+    }
 }
 
 function render() {
@@ -161,11 +183,12 @@ function render() {
 }
 
 function renderText() {
-    ctx.font = `${FONT_SIZE}px "${FONT_FAMILY}"`;
+    ctx.font = `${WIDTH / 25}px "${FONT_FAMILY}"`;
     ctx.fillStyle = `hsl(${FONT_COLOR_HLS},100%,50%)`;
     ctx.shadowColor = `black`;
-    const textWidth = ctx.measureText(TEXT).width;
-    ctx.fillText(TEXT, (WIDTH - textWidth)/2, HEIGHT / 3)
+    let text = TEXT.substr(0, TEXT_LENGTH) + (SHOW_SYMBOL ? '|' : TEXT.length === TEXT_LENGTH ? ' ' : '');
+    const textWidth = ctx.measureText(text).width;
+    ctx.fillText(text, (WIDTH - textWidth)/2, HEIGHT / 3)
 }
 
 function renderTree() {
@@ -218,3 +241,22 @@ function strokeStar(x, y, r, n, inset) {
     ctx.stroke();
     ctx.restore();
 }
+
+window.addEventListener('resize', () => {
+    WIDTH = canvas.width = window.innerWidth;
+    HEIGHT = canvas.height = window.innerHeight;
+    snowflakes = createSnow(SNOWFLAKES_COUNT);
+})
+
+const textInput = document.getElementById('textInput');
+textInput.value = TEXT;
+textInput.addEventListener('change', (e) => {
+    TEXT = e.target.value;
+    TEXT_LENGTH = 0;
+})
+
+const textIntervalInput = document.getElementById('textIntervalInput');
+textIntervalInput.value = TEXT_INPUT_INTERVAL;
+textIntervalInput.addEventListener('change', (e) => {
+    TEXT_INPUT_INTERVAL = e.target.value;
+})
